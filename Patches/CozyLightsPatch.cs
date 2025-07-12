f0ef713693177396f36fe6cdde285283cf5bfc84
Duttbust <87608404+Duttbust@users.noreply.github.com>
2025-07-12 16:23:22 -0400

    Add files via upload

---
+++ Patches/CozyLightsPatch.cs
@@ -0,0 +1,19 @@
+﻿using BIGSHOTDISCO;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LethalCompanyTemplate.Patches
+{
+    [HarmonyPatch(typeof(CozyLights))]
+    internal class CozyLightsPatch
+    {
+        [HarmonyPatch("SetAudio")]
+        [HarmonyPostfix]
+        static void patchFunction(CozyLights __instance)
+        {
+            __instance.turnOnAudio.clip = Mod.sounds[0];
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalCompanyTemplate.Patches;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BIGSHOTDISCO
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Mod : BaseUnityPlugin
+    {
+        private const string modGUID = "Duttbust.BIGSHOTDISCO";
+        private const string modName = "BIGSHOTDISCO";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        internal static Mod Instance;
+
+        internal ManualLogSource mls;
+
+        internal static List<AudioClip> sounds;
+        internal static AssetBundle bundle;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            if ( Instance == null )
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            harmony.PatchAll(typeof(CozyLightsPatch));
+
+            sounds = new List<AudioClip>();
+            string FolderLocation = Instance.Info.Location;
+            FolderLocation = FolderLocation.TrimEnd("BIGSHOTDISCO.dll".ToCharArray());
+            bundle = AssetBundle.LoadFromFile(FolderLocation + "music");
+            if( bundle != null)
+            {
+                mls.LogInfo("Loaded AssetBundle!");
+                sounds = bundle.LoadAllAssets<AudioClip>().ToList();
+            }
+            else
+            {
+                mls.LogError("Failed to load AssetBundle");
+            }
+            
+        }
+    }
+}
